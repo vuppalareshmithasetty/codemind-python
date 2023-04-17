035da181d7c6ef2203c5ee6ecc70d7cbfd43f66d
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-04-17 11:21:18 +0530

    r,c=map(int,input().split()) mat=[] s=0 q=0 for i in range(r):     sub_list=list(map(int,input().split()))     mat.append(sub_list) for i in range(r):     for j in range(c):         if i%2==0:             s=s+mat[i][j]         else:             q=q+mat[i][j] print(s,q)

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
@@ -0,0 +1,14 @@
+r,c=map(int,input().split())
+mat=[]
+s=0
+q=0
+for i in range(r):
+    sub_list=list(map(int,input().split()))
+    mat.append(sub_list)
+for i in range(r):
+    for j in range(c):
+        if i%2==0:
+            s=s+mat[i][j]
+        else:
+            q=q+mat[i][j]
+print(s,q)
