d0581c7fcc87e18632551c5808927383df978e94
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-08-22 19:11:33 +0530

    def compound_interest(principal, rate, time): 	Amount = principal * (pow((1 + rate / 100), time)) 	CI = Amount 	print('%.2f' % CI) #	print( round(CI, 2)) principal,rate,time = map(int,input().split()) compound_interest(principal,rate,time)

--- Compound_interest.py
+++ Compound_interest.py
@@ -1,9 +1,7 @@
 def compound_interest(principal, rate, time):
-    Amount = principal * (pow((1 + rate / 100), time))
-    CI = Amount
-    print("%.2f"%(CI))
-  
-  
-principal,rate,time=map(int,input().split())
+	Amount = principal * (pow((1 + rate / 100), time))
+	CI = Amount
+	print('%.2f' % CI)
+#	print( round(CI, 2))
+principal,rate,time = map(int,input().split())
 compound_interest(principal,rate,time)
-  
