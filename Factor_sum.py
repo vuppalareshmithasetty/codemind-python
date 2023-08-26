51241cdf342a91f9c7566c4a06efb231a3a47aee
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-08-26 22:04:20 +0530

    l=list(map(int,input().split(','))) k=0 s=[] c=0 for i in l:    k=0    for j in range(1,i+1):        if(i%j==0):            k=k+j    if k in l:        s.append(i)        c+=1 if(c==0):     print("-1") else:     s=sorted(s)     print(*s)

--- Factor_sum.py
+++ Factor_sum.py
@@ -3,14 +3,14 @@ k=0
 s=[]
 c=0
 for i in l:
-    k=0
-    for j in range(1,i+1):
-        if(i%j==0):
-            k=k+j
-    if k in l:
-        s.append(i)
-        c+=1
-if (c==0):
+   k=0
+   for j in range(1,i+1):
+       if(i%j==0):
+           k=k+j
+   if k in l:
+       s.append(i)
+       c+=1
+if(c==0):
     print("-1")
 else:
     s=sorted(s)
