451669760acdaf21527374a9f6be9bbea3624e5c
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-10-22 12:42:12 +0530

    n=int(input()) l=list(map(int,input().split())) c=0 for i in range(n):     if l[i]%2==0:         ff=i         break for i in range(n-1,0,-1):     if l[i]%2==0:         ll=i         break for i in range(n):     if i>ff and i<ll:         if l[i]%2==0:             c=c+1 print(c)

---
+++ count_even_numbers_between_even_numbers.py
@@ -0,0 +1,16 @@
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in range(n):
+    if l[i]%2==0:
+        ff=i
+        break
+for i in range(n-1,0,-1):
+    if l[i]%2==0:
+        ll=i
+        break
+for i in range(n):
+    if i>ff and i<ll:
+        if l[i]%2==0:
+            c=c+1
+print(c)
