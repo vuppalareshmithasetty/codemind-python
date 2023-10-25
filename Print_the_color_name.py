2686b49d7e83c9575a541b53db594dd068f066ef
Vuppala Reshmitha Setty <120186084+vuppalareshmithasetty@users.noreply.github.com>
2023-10-25 21:21:08 +0530

    n=input() if n=="V":     print("Violet") elif n=="I":     print("Indigo") elif n=="B":     print("Blue") elif n=="G":     print("Green") elif n=="Y":     print("Yellow") elif n=="O":     print("Orange") elif n=="R":     print("Red") else:     print("-1")

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+n=input()
+if n=="V":
+    print("Violet")
+elif n=="I":
+    print("Indigo")
+elif n=="B":
+    print("Blue")
+elif n=="G":
+    print("Green")
+elif n=="Y":
+    print("Yellow")
+elif n=="O":
+    print("Orange")
+elif n=="R":
+    print("Red")
+else:
+    print("-1")
